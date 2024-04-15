11b0fe946a6b99a674610778ddeedc2b1d392df7
Luiz Antonio <luiz.antonio.da.silva777@gmail.com>
2024-04-15 13:17:30 -0300

    Adição da estrutura das notícias sobre Ciencia

--- src/app/Ciencia/Arevolucaodospacientesquepodemelhoraramedicina/page.tsx
+++ src/app/Ciencia/Arevolucaodospacientesquepodemelhoraramedicina/page.tsx
@@ -3,7 +3,42 @@
 export default function noticia() {
     return (
         <>
-           
+            <h1>A revolução dos pacientes que pode melhorar a medicina</h1>
+            <p>
+                Livro 'Rebel health' ('Saúde rebelde') mostra que avanços no tratamento de doenças raras ou crônicas partiram da 
+                mobilização das próprias pessoas ou de seus cuidadores.
+            </p>
+            <p>
+                Com a experiência de quem esteve à frente da área de tecnologia do Departamento de Saúde e Serviços Humanos dos EUA, 
+                responsável por zelar pelo bem-estar dos norte-americanos, Susannah Fox decidiu auxiliar as pessoas a transitar no 
+                árido território que permite um melhor acesso à saúde. Ela acaba de lançar “Rebel health” (em tradução livre, “Saúde 
+                rebelde”), livro que mapeia o esforço de pacientes, familiares e cuidadores que, através de comunidades de apoio, 
+                muitas vezes conseguem produzir avanços no tratamento de enfermidades que não ganham a mesma atenção que as mais 
+                conhecidas e pesquisadas, como os diversos tipos de câncer.
+            </p>
+            <img src="/assets/imagens_ciencia/Susannah_Fox.png" alt="imagem de Susannah Fox"/>
+            <p>
+                De diabetes a síndromes pouco conhecidas, a autora chama a atenção para a dedicação e persistência desses grupos 
+                que, em suas palavras, são capazes de criar a “revolução dos pacientes”. O trabalho incansável das pessoas em fóruns 
+                on-line se tornou seu objeto de estudo. Há cerca de sete mil doenças raras já mapeadas e 300 milhões de portadores 
+                dessas enfermidades no mundo, sendo que 13 milhões no Brasil.
+            </p>
+            <p>
+                “Eu me dei conta da dimensão do problema quando, há cerca de 20 anos, comecei a conversar com pacientes que não 
+                tinham um diagnóstico, ou viviam com alguma doença rara, que não tinham a oportunidade de conhecer outros com a 
+                mesma condição, nem a possibilidade de discutir alternativas de tratamento. Daí a importância da internet para 
+                conectar, trocar informações e dar apoio a essas pessoas. A necessidade dos testes genéticos começou nas comunidades 
+                on-line uns dez anos antes do sistema de saúde entrar no assunto”, afirmou em entrevista.
+            </p>
+            <p>
+                No livro, a história da Diabetes Rebel Alliance (Aliança Rebelde do Diabetes) é uma das preferidas de Susannah: 
+                “eles pesquisam, compartilham, propõem soluções, trabalham juntos em prol de mudanças e têm acesso ao círculo dos 
+                gestores de políticas públicas”. Ela também resgata o engajamento dos Panteras Negras para dar mais visibilidade à 
+                anemia falciforme, que atinge numa proporção muito maior a população negra, com bons resultados: a oferta de testes 
+                para serem feitos em casa e investimentos para o estudo da doença. “Eu acredito que a tecnologia produz coisas boas 
+                para os indivíduos. Tendo trabalhado no governo (de Barack Obama), pude ver de perto o gigantismo do sistema de 
+                saúde, as mudanças têm que acontecer lenta e cuidadosamente, para não haver uma ruptura”, finalizou.
+            </p>
         </>
     );
 }
--- src/app/Ciencia/GravidezdearraiasemcontatocommachosurpreendeemaquariodosEUAentenda/page.tsx
+++ src/app/Ciencia/GravidezdearraiasemcontatocommachosurpreendeemaquariodosEUAentenda/page.tsx
@@ -3,7 +3,44 @@
 export default function noticia() {
     return (
         <>
-           
+            <h1>Gravidez de arraia sem contato com macho surpreende em aquário dos EUA; entenda</h1>
+            <img src="/assets/imagens_ciencia/Arraia.png" alt="imagem de Arraia"/>
+            <p>
+                Charlotte, como é conhecida, apareceu grávida mesmo sem ter contato com macho da mesma espécie há oito anos. Caso 
+                chamou atenção e transformou peixe em estrela
+            </p>
+            <p>
+                Uma arraia que está grávida no Aquarium & Shark Lab Team ECCO na Carolina do Norte, nos Estados Unidos, está gerando 
+                discussões na comunidade científica com relação à paternidade dos quatro filhotes. Isso porque a nova mãe não tem 
+                contato com machos da mesma espécie há oito anos. Charlotte, como é chamada pelos funcionários do aquário, divide o 
+                tanque com tubarões, o que iniciou as especulações sobre híbridos das duas espécies.
+            </p>
+            <p>
+                Maristela Peres Rangel, bióloga professora e mentora do Colégio Mackenzie - Tamboré, explica que o cruzamento e 
+                fecundação entre animais distintos é possível, mas a tendência é que este não seja o caso dos animais do aquário 
+                americano. “Só conseguimos filhotes híbridos com espécies muito próximas como leões e tigres, mas o que sai dessa 
+                reprodução é infértil, a espécie não se perpetua. Não é o que aconteceu entre a arraia e o tubarão”.
+            </p>
+            <p>
+                No caso de Charlotte, nos Estados Unidos, Maristela acredita se tratar de um caso de reprodução por partenogênese. 
+                “É um processo de reprodução assexuada em que algumas fêmeas, em função do ambiente em que estão, acabam gerando 
+                descendentes sozinhas”, explica.
+            </p>
+            <p>
+                Ronaldo Christofoletti, professor do Instituto Mar da Universidade Federal de São Paulo (Unifesp), acrescenta que 
+                em casos de partenogênese “apenas o óvulo da fêmea é capaz de, nas suas divisões, acabar gerando um novo indivíduo. 
+                É raro para espécies que se reproduzem com machos e fêmeas, mas nessa raridade é possível acontecer”, afirma.
+            </p>
+            <p>
+                Os animais tendem a preterir este tipo de reprodução pela baixa chance de manutenção da vida dos filhotes. “Os 
+                animais que vão ser gerados à partir da partenogênese não tem variabilidade genética, se tiver é uma variação 
+                mínima, a falta dessa variabilidade é um problema de sobrevivência com a passagem dos anos”, segundo Maristela.
+            </p>
+            <p>
+                O anúncio da gravidez foi feito no início do mês nas redes sociais do aquário que realizou transmissões ao vivo 
+                para compartilhar o ultrassom do animal. Os biólogos responsáveis atualizam as redes com informações de Charlotte 
+                que ainda não deu à luz aos filhotes, evento que é aguardado para realização de testes genéticos.
+            </p>
         </>
     );
 }
