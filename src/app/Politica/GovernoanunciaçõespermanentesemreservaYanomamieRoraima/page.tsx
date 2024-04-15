aac3ceaf5d0cec436d48218d5b5b0448e50a6b7d
Luiz Antonio <luiz.antonio.da.silva777@gmail.com>
2024-04-14 22:10:39 -0300

    Adicção da estrutura das notícias de politica

--- src/app/Politica/GovernoanunciaçõespermanentesemreservaYanomamieRoraima/page.tsx
+++ src/app/Politica/GovernoanunciaçõespermanentesemreservaYanomamieRoraima/page.tsx
@@ -3,7 +3,60 @@
 export default function noticia() {
     return (
         <>
-           
+            <h1>Governo anuncia ações permanentes em reserva Yanomami em Roraima</h1>
+            <p>Medidas englobam mudanças na atuação dos militares na região e coordenação política interministerial</p>
+            <img src="/assets/imagens_politica/mulher_com_criança.png" alt="imagem de mulher com criança" />
+            <p>
+                O governo federal anunciou que as ações da União na Reserva Indígena Yanomami passarão de emergenciais para 
+                permanentes.
+
+                Entre as medidas está a criação de uma “casa de governo“, órgão que representará o executivo federal no estado de 
+                Roraima e vai congregar representantes de diversos ministérios, além de uma nova organização de logística de 
+                atendimento e segurança pública.
+            </p>
+            <p>
+                O custo das medidas para 2024 é estimado em R$1,2 bilhão de reais e deverá ser feito por meio de crédito extraordinário.
+            </p>
+            <p>
+                Segundo o Ministro da Casa Civil, Rui Costa, a “casa de governo” não é uma intervenção, mas um conceito que está 
+                em construção e também terá diálogo com municípios e o governo estadual.
+            </p>
+            <p>
+                A intenção é estudar também alternativas econômicas para a população que vive direta ou indiretamente do garimpo.
+            </p>
+            <p>
+                “Não se trata de uma intervenção, se trata de uma casa de governo que vai cotidianamente e não, esporadicamente, 
+                tratar a questão”, explicou Costa.
+            </p>
+            <p>
+                Ainda de acordo com o governo, as Forças Armadas deixarão de dar suporte logístico para envio de ajuda humanitária 
+                e saúde e devem se concentrar, em conjunto com a Polícia Federal, em ações de segurança pública.
+            </p>
+            <p>
+                Para isso, o Ministério de Gestão e Inovação de Serviços Públicos deve apresentar, até março, uma proposta de um 
+                contrato a longo prazo para retirar essas atribuições dos militares.
+            </p>
+            <p>
+                “Em 30 dias, vamos, com o Ministério da Defesa e a Polícia Federal, reestruturar a ocupação das Forças Armadas e 
+                da PF na região. Não de forma provisória, mas com presença definitiva… Será uma nova ocupação do território na 
+                fronteira do Brasil que será feita”, afirmou o ministro.
+            </p>
+            <p>
+                As ações foram divulgadas nesta terça-feira (09), depois de uma reunião de diversos ministérios e órgãos ambientais 
+                com o presidente Luiz Inácio Lula da Silva.
+            </p>
+            <p>
+                Há cerca de um ano, o governo decretou emergência em saúde pública para conter a crise de falta de assistência aos 
+                indígenas. Na época, Lula visitou um ponto da reserva.
+            </p>
+            <p>
+                O ministro Rui Costa admitiu que apesar dos números do governo apontarem para uma diminuição das invasões de 
+                garimpo e do crime organizado, o problema persiste na região.
+            </p>
+            <p>
+                O governo afirmou que no último ano, as ações emergenciais somam a apreensão e destruição do equivalente a R$ 
+                600 milhões em patrimônios utilizados em crimes, como embarcações e maquinário.
+            </p>
         </>
     );
 }
--- src/app/Politica/Vozesaimportanciadarepresentatividadefemininanapolitica/page.tsx
+++ src/app/Politica/Vozesaimportanciadarepresentatividadefemininanapolitica/page.tsx
@@ -3,7 +3,73 @@
 export default function noticia() {
     return (
         <>
-           
+           <h1>Vozes: a importância da representatividade feminina na política</h1>
+           <p>
+                O Censo 2022 mostra que a população brasileira é composta em sua maioria por mulheres e esse percentual deveria estar 
+                refletido também nos cargos políticos de decisão
+           </p>
+           <img src="/assets/imagens_politica/censo2022.png" alt="imagem sobre o censo" />
+           <p>
+                Na semana passada novos dados do Censo 2022, do Instituto Brasileiro de Geografia e Estatística (IBGE), foram 
+                divulgados e um, em particular, me fez refletir. Para cada 100 mulheres no Brasil, há aproximadamente 94 homens. 
+                Esses números representam uma diferença significativa de gênero na população brasileira, com as mulheres compondo 
+                51,5% do total, o que se traduz em seis milhões a mais de mulheres do que homens. 
+           </p>
+           <p>
+                Os dados apresentados não apenas refletem uma realidade demográfica, mas também levantam questões importantes sobre 
+                a falta de representatividade feminina na política e como isso pode impactar a sociedade como um todo. 
+           </p>
+           <p>
+                A representatividade na política é um aspecto fundamental de qualquer sociedade. Ela permite que diferentes grupos 
+                e comunidades tenham suas vozes ampliadas e suas necessidades atendidas. E neste contexto do Brasil, ter mais 
+                mulheres na política é uma questão fundamental. Afinal, como podemos querer uma sociedade justa e equitativa 
+                se as mulheres não têm uma presença significativa nos espaços de tomada de decisão? Somos apenas 17,7% dos 513 
+                deputados federais, na Câmara dos Deputados. No Senado, temos hoje, 15 senadoras dentre os 81 parlamentares, um 
+                percentual de representatividade de 18,5%. 
+           </p>
+           <p>
+                O Censo 2022 mostra claramente que as mulheres são uma parte substancial da população brasileira. Portanto, é 
+                inegável que haja um esforço contínuo para aumentar a participação das mulheres na política. Não se trata apenas 
+                de preencher cotas de gênero, mas de garantir que as vozes, preocupações e perspectivas das mulheres sejam ouvidas 
+                e consideradas nas políticas públicas, inclusive, que seus direitos sejam defendidos. 
+           </p>
+           <h2>Mais mulheres em espaços de poder: qual o impacto gerado?</h2>
+           <p>
+                Como ponto de partida para responder a esse questionamento, podemos sinalizar o dado o qual mostra que, quando 
+                mulheres estão ocupando posições de liderança no setor público, existe uma probabilidade 35% menor de envolvimento 
+                em casos de corrupção, em comparação aos homens, é o que aponta um levantamento conduzido pelo Núcleo de Estudos 
+                Raciais do Insper, de 2023.
+           </p>
+           <p>
+                Essa relação entre representatividade feminina e a diminuição dos níveis de corrupção também foi identificada 
+                em um estudo que abrangeu mais de 100 países em 2001. Nele foi observado que uma maior presença de mulheres 
+                no governo estava associada a índices mais baixos de corrupção.
+           </p>
+           <p>
+                Os pesquisadores do Núcleo de Estudos Raciais revisaram 95 estudos publicados em livros e revistas acadêmicas 
+                ao longo das últimas duas décadas, tanto nacionais quanto internacionais, que abordaram a diversidade de gênero 
+                e raça na política e no mercado de trabalho.
+           </p>
+           <p>
+                Além disso, de acordo com as descobertas do Insper, mulheres ocupando cargos de liderança política tendem a alocar 
+                até 7% a mais de recursos em bens públicos, como saúde e educação, em comparação com seus colegas do sexo masculino. Isso resulta em uma redução de até 24% na taxa de mortalidade infantil e uma diminuição de até 32% na disparidade de gênero no acesso à educação entre adolescentes, bem como uma redução de 25% na disparidade de gênero entre adultos. 
+           </p>
+           <p>
+                Líderes femininas demonstram maior preocupação em propor e implementar políticas direcionadas à saúde de mulheres 
+                e crianças quando ocupam cargos em legislaturas estaduais. Essa abordagem tem um impacto positivo na qualidade dos 
+                serviços de saúde pré-natal e infantil em suas respectivas regiões eleitorais. 
+           </p>
+           <p>
+                Os dados do Censo 2022, que destacam a maioria feminina na população brasileira, devem servir como um lembrete 
+                claro da necessidade de maior representatividade feminina na política, e claro sempre pensando na perspectiva de 
+                raça e território. Por isso, precisamos assegurar que as mulheres tenham um papel ativo na formulação de políticas 
+                e nas tomadas de decisões que afetam suas vidas e a sociedade como um todo.
+           </p>
+           <p>
+                Ao abraçar a diversidade de perspectivas e experiências, melhoramos a qualidade de nossas políticas e promovemos 
+                um futuro mais igualitário e próspero. Já passou da hora de darmos espaço e apoio às mulheres que desejam liderar, 
+                para que possamos construir um Brasil mais inclusivo e equitativo, agora precisamos agir e agir coletivamente. 
+           </p>
         </>
     );
 }
