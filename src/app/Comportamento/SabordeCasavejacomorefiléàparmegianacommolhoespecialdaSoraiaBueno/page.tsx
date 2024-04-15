fd5ec7c15967453c027768a22ae52179398c606c
Luiz Antonio <luiz.antonio.da.silva777@gmail.com>
2024-04-15 11:19:57 -0300

    Adição da estrutura das notícias sobre Comportamento

--- src/app/Comportamento/SabordeCasavejacomorefiléàparmegianacommolhoespecialdaSoraiaBueno/page.tsx
+++ src/app/Comportamento/SabordeCasavejacomorefiléàparmegianacommolhoespecialdaSoraiaBueno/page.tsx
@@ -3,7 +3,114 @@
 export default function noticia() {
     return (
         <>
-           
+            <h1>Sabor de Casa: veja como fazer filé à parmegiana com 'molho especial' da Soraia Bueno</h1>
+            <p>
+                Empresária de Mogi Guaçu (SP) ensina versão que traz no molho alho-poró, molho de soja, mostarda e páprica picante 
+                como diferenciais.
+            </p>
+            <p>
+                O filé à parmegiana figura em cardápios de restaurantes e bares Brasil afora, e também é figurinha carimbada nas refeições de muitas famílias. Mas que 
+                tal dar um toque diferente a um preparo tão tradicional?
+                O quadro “Sabor de Casa” que vai ao ar no EPTV 1 nesta quarta-feira (24) traz uma receita cujo destaque está no molho.
+            </p>
+            <p>O filé à parmegiana da empresária Soraia Bueno, de Mogi Guaçu (SP), tem no molho alho-poró, molho de soja, mostarda e páprica picante como diferenciais.</p>
+            <p>
+                "A diferença nessa receita é meu molho, bem rústico, supersimples de fazer, e o sabor é maravilhoso", avisa.
+                Soraia conta que uma de suas paixões é cozinhar nas horas vagas, e ela costuma fazer sucesso nas redes sociais com as receitas que prepara.
+
+            </p>
+            <img src="/assets/imagens_comportamento/File_a_parmegiana.png" alt="imagem de File_a_parmegiana" />
+            <h2>Filé à parmegiana da Soraia Bueno</h2>
+            <p>
+                Ingredientes
+
+
+                Filé 🥩
+                1,5 kg de filé mignon
+                1 xícara farinha trigo
+                1 xícara farinha rosca
+                100 gramas parmesão ralado
+                4 ovos
+
+
+                Molho 🍅
+                2 latas de tomates pelados
+                2 cenouras médias
+                1 xícara de alho-poró picado
+                1/2 pimentão vermelho
+                1/2 pimentão amarelo
+                1/2 pimentão verde
+                1,5 kg de tomate sem semente
+                1 cebola grande
+
+
+                Temperos 🧄
+                1 xícara azeite
+                6 dentes de alho
+                1 e 1/2 colher de mostarda
+                1 e 1/2 colher de molho de soja
+                1 colher de sopa de páprica picante
+                1 colher de sopa de orégano
+                1 colher de sopa de pimenta calabresa
+                Sal a gosto
+
+
+                Finalização 🧀
+                Queijo mussarela a gosto.
+
+            </p>
+            <h2>Modo de preparo 🍽</h2>
+            <p>
+                Molho 🍅
+                Em uma panela acrescente os tomates, pimentões, a cebola, as cenouras, o alho-poró e deixe ferver por aproximadamente 20 minutos, até os ingredientes murcharem;
+                Assim que estiverem “mais molinhos”, bata no liquidificador ainda quente;
+                Em uma panela, frite o alho com azeite;
+                Acrescente o molho batido no liquidificador na panela e deixe ferver por 10 minutos;
+                Tempere com páprica, orégano, molho de soja, mostarda e sal.
+
+
+                Filés empanados 🥩
+                Em uma vasilha quebre os ovos e acrescente sal e pimenta a gosto;
+                Em outro recipiente coloque as duas farinhas, o parmesão, o sal e misture;
+                Na sequência salpique sobre os bifes um pouco de sal e páprica;
+                Passe os bifes na farinha, em seguida no ovo, e na sequência na farinha novamente;
+                Frite os filés em óleo.
+
+
+                Finalização 👇
+                Coloque em um refratário os filés fritos, e por cima despeje o molho preparado;
+                Finalize com queijo muçarela ralado por cima;
+                Leve ao forno por 20 minutos;
+                Sirva com arroz branco, batas rústicas e/ou fritas.
+            </p>
+            <h2>Pãozinho de tapioca sem glúten</h2>
+            <p>
+                O pãozinho de tapioca sem glúten é uma opção saudável para o café da manhã ou lanche da tarde. Com poucos 
+                ingredientes, a receita rende até 20 unidades.
+            </p>
+            <p>
+                Para preparar, você vai precisar de tapioca granulada, polvilho doce, queijo parmesão e outros ingredientes fáceis 
+                de encontrar. Veja como fazer!
+            </p>
+            <p>
+                Ingredientes
+                Pãozinho de tapioca sem glúten
+                2 xícaras de chá de leite
+                4 colheres de sopa de óleo
+                Sal
+                1 xícara de chá de tapioca granulada
+                1 ovo
+                150 gramas de parmesão ralado
+                2 colheres de sopa de polvilho doce
+                Manteiga para untar
+                Modo de Preparo
+                Pãozinho de tapioca sem glúten
+                1 Leve o leite com o óleo e sal a gosto ao fogo médio e mexa até ferver.
+                2 Despeje sobre a tapioca granulada, mexendo rapidamente para escaldar, e deixe esfriar.
+                3 Acrescente o ovo, 4 colheres de sopa do parmesão e o polvilho e misture bem.
+                4 Modele bolinhas com pequenas porções da massa, envolva no restante do parmesão e disponha em assadeira untada com manteiga.
+                5 Asse em forno médio preaquecido a 180 graus Celsius por cerca de 30 minutos ou até dourarem. Sirva em seguida.
+            </p>
         </>
     );
 }
