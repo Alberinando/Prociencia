79d22f27eb325ce7d0c3ccdf40efc238946e342f
Luiz Antonio <luiz.antonio.da.silva777@gmail.com>
2024-09-22 23:21:34 -0300

    refactor: Utilização de componente Newlatter em paginas de comportamento

--- src/app/Comportamento/SabordeCasavejacomorefileaparmegianacommolhoespecialdaSoraiaBueno/page.tsx
+++ src/app/Comportamento/SabordeCasavejacomorefileaparmegianacommolhoespecialdaSoraiaBueno/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Newsletter from "@/components/Newsletter/Newsletter"
 
 export default function noticia() {
     return (
@@ -33,24 +34,7 @@ export default function noticia() {
            
 
             <div className="grow overflow-hidden   sm:col-start-6 col-span-3 col-start-2 col-span-4 space-y-4   m-0 sm:px-[50px] md:px-[60px] lg:px-[70px] xl:px-[80px]">
-                <div className="bg-darkgray flex justify-center ">
-                    <h1 className="text-[15px] sm:text-[20px] md:text-[25px] lg:text-[27px] xl:text-[32px] text-center self-center  bg-darkgray">NEWSLETTER HORÓSCOPO</h1>
-                </div>
-                    <p className=" text-[18px] ">Toda segunda, receba no seu e-mail as previsões semanais para o seu signo.</p>
-                    <p className="text-center ">Cadastre o seu melhor e-mail:</p>
-                       
-                    <div className="">
-                        <input
-                                type="text"
-                                className=" w-full p-2 border border-gray rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                placeholder="Digite aqui..."
-                            />
-                    </div>
-                    <div className="flex justify-center ">
-                        <button className="bg-gray mt-4 w-48 h-10 col-start-2 col-span-1 font-pixels">
-                            Enviar
-                        </button>
-                    </div>
+               <Newsletter/>
 
             </div>
 
@@ -155,140 +139,6 @@ export default function noticia() {
                 <div className=" hidden sm:block h-[1666px]  bg-darkgray "></div>
             </div>
         </div>
-
-
-    
-           { /*<h1>Sabor de Casa: veja como fazer filé à parmegiana com 'molho especial' da Soraia Bueno</h1>
-            <p>
-                Empresária de Mogi Guaçu (SP) ensina versão que traz no molho alho-poró, molho de soja, mostarda e páprica picante 
-                como diferenciais.
-            </p>
-            <p>
-                O filé à parmegiana figura em cardápios de restaurantes e bares Brasil afora, e também é figurinha carimbada nas refeições de muitas famílias. Mas que 
-                tal dar um toque diferente a um preparo tão tradicional?
-                O quadro “Sabor de Casa” que vai ao ar no EPTV 1 nesta quarta-feira (24) traz uma receita cujo destaque está no molho.
-            </p>
-            <p>O filé à parmegiana da empresária Soraia Bueno, de Mogi Guaçu (SP), tem no molho alho-poró, molho de soja, mostarda e páprica picante como diferenciais.</p>
-            <p>
-                "A diferença nessa receita é meu molho, bem rústico, supersimples de fazer, e o sabor é maravilhoso", avisa.
-                Soraia conta que uma de suas paixões é cozinhar nas horas vagas, e ela costuma fazer sucesso nas redes sociais com as receitas que prepara.
-
-            </p>
-            <img src="/assets/imagens_comportamento/File_a_parmegiana.png" alt="imagem de File_a_parmegiana" />
-            <h2>Filé à parmegiana da Soraia Bueno</h2>
-            <p>
-                Ingredientes
-
-
-                Filé 🥩
-                1,5 kg de filé mignon
-                1 xícara farinha trigo
-                1 xícara farinha rosca
-                100 gramas parmesão ralado
-                4 ovos
-
-
-                Molho 🍅
-                2 latas de tomates pelados
-                2 cenouras médias
-                1 xícara de alho-poró picado
-                1/2 pimentão vermelho
-                1/2 pimentão amarelo
-                1/2 pimentão verde
-                1,5 kg de tomate sem semente
-                1 cebola grande
-
-
-                Temperos 🧄
-                1 xícara azeite
-                6 dentes de alho
-                1 e 1/2 colher de mostarda
-                1 e 1/2 colher de molho de soja
-                1 colher de sopa de páprica picante
-                1 colher de sopa de orégano
-                1 colher de sopa de pimenta calabresa
-                Sal a gosto
-
-
-                Finalização 🧀
-                Queijo mussarela a gosto.
-
-            </p>
-            <h2>Modo de preparo 🍽</h2>
-            <p>
-                Molho 🍅
-                Em uma panela acrescente os tomates, pimentões, a cebola, as cenouras, o alho-poró e deixe ferver por aproximadamente 20 minutos, até os ingredientes murcharem;
-                Assim que estiverem “mais molinhos”, bata no liquidificador ainda quente;
-                Em uma panela, frite o alho com azeite;
-                Acrescente o molho batido no liquidificador na panela e deixe ferver por 10 minutos;
-                Tempere com páprica, orégano, molho de soja, mostarda e sal.
-
-
-                Filés empanados 🥩
-                Em uma vasilha quebre os ovos e acrescente sal e pimenta a gosto;
-                Em outro recipiente coloque as duas farinhas, o parmesão, o sal e misture;
-                Na sequência salpique sobre os bifes um pouco de sal e páprica;
-                Passe os bifes na farinha, em seguida no ovo, e na sequência na farinha novamente;
-                Frite os filés em óleo.
-
-
-                Finalização 👇
-                Coloque em um refratário os filés fritos, e por cima despeje o molho preparado;
-                Finalize com queijo muçarela ralado por cima;
-                Leve ao forno por 20 minutos;
-                Sirva com arroz branco, batas rústicas e/ou fritas.
-            </p>
-            <h2>Pãozinho de tapioca sem glúten</h2>
-            <p>
-                O pãozinho de tapioca sem glúten é uma opção saudável para o café da manhã ou lanche da tarde. Com poucos 
-                ingredientes, a receita rende até 20 unidades.
-            </p>
-            <p>
-                Para preparar, você vai precisar de tapioca granulada, polvilho doce, queijo parmesão e outros ingredientes fáceis 
-                de encontrar. Veja como fazer!
-            </p>
-            <p>
-                Ingredientes
-                Pãozinho de tapioca sem glúten
-                2 xícaras de chá de leite
-                4 colheres de sopa de óleo
-                Sal
-                1 xícara de chá de tapioca granulada
-                1 ovo
-                150 gramas de parmesão ralado
-                2 colheres de sopa de polvilho doce
-                Manteiga para untar
-                Modo de Preparo
-                Pãozinho de tapioca sem glúten
-                1 Leve o leite com o óleo e sal a gosto ao fogo médio e mexa até ferver.
-                2 Despeje sobre a tapioca granulada, mexendo rapidamente para escaldar, e deixe esfriar.
-                3 Acrescente o ovo, 4 colheres de sopa do parmesão e o polvilho e misture bem.
-                4 Modele bolinhas com pequenas porções da massa, envolva no restante do parmesão e disponha em assadeira untada com manteiga.
-                5 Asse em forno médio preaquecido a 180 graus Celsius por cerca de 30 minutos ou até dourarem. Sirva em seguida.
-            </p>
-            <div className="grid justify-items-center w-6/8 space-y-[100px]">
-                <div className="w-2/8 ml-[50px]">
-                        
-                    <div className="w-[419px] h-[538px] mt-[400px] space-y-6">
-                        <h1 className="w-[419px] h-[108px]  bg-darkgray text-[36px] text-center">NEWSLETTER HORÓSCOPO</h1>
-                        <p className="w-[419px] h-[108px] text-[26px]  text-black">Toda segunda, receba no seu e-mail as previsões semanais para o seu signo.</p>
-                        <p>Cadastre o seu melhor e-mail:</p>
-                        <input
-                        type="text"
-                        className="w-[356px] h-[30px] w-full p-2 border border-gray rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        placeholder="Digite aqui..."
-                        />
-                        <button className="bg-gray mt-4 ml-[100px] w-48 h-10 font-pixels">
-                            Enviar
-                        </button>
-                    </div>
-                    <div className="h-[400px] w-[403px]">
-                        <div className="w-[419px] h-[1666px]  bg-darkgray">
-                            <p className=" text-[64px]">Área para Para divulgação</p>
-                        </div>
-                    </div>
-                </div>
-            </div>*/}
         </>
     );
 }
